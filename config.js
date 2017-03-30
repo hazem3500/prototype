e1d27502589e28c47430afbadcebd38560fab77f
Hussein <husseinraoouf@gmail.com>
2017-03-30 12:29:34 +0200

    All Code

--- config.js
+++
@@ -1,4 +0,0 @@
-window.GoogleSamples = window.GoogleSamples || {};
-window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AAAAFeZx2QA:APA91bERPTMD47thTNv539qiBib7jSSSS-sIDTgOLS_ZhiGteeYhbdAbTp7dlLBlhLFZKNfByfj1xn3R1yWrfUrqrln0OdjBYwvRpgIr2PuahpdXkxopIVX9jydDPq_02cvfKHA76bNp'
-};
