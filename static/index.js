85d49f1b5b423593d1189b47a551b9f56f58e7a5
hazem osama ibrahim <hazem3500@gmail.com>
2017-03-31 00:40:26 +0200

    Update index.js

--- static/index.js
+++ static/index.js
@@ -2,4 +2,4 @@ var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', 
 
 setInterval(function() {
     $("body").css("background-color",colors[colors.push(colors.shift())-1]);
-}, 6000);
+}, 600000);
